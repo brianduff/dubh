1330c37a9a214ca58409be5e1000ec05a2f794da
Brian Duff <cairndubh@gmail.com>
1999-03-13 01:33:32 +0000

    Add support for remembering position of debug window.
    Allow asserts to be switched off.
    
    Add methods to get the current trace level and whether asserts
    are on.

--- dju/src/org/dubh/dju/misc/Debug.java
+++ dju/src/org/dubh/dju/misc/Debug.java
@@ -47,7 +47,9 @@ import dubh.utils.ui.OutputStreamFrame;
  @author Brian Duff
  @version 1.2
  */
-public class Debug {
+public class Debug {  
+
+   private static final String DEBUG_WINDOW = "debugout";
 
    /**
     * Level 3 is for low level, unimportant debug (e.g. diagnostics)
@@ -68,6 +70,7 @@ public class Debug {
     */
    public static final boolean ASSERT = true;
 
+   private static boolean m_assert = true;
    private static boolean m_showmessage = true;
    private static PrintWriter m_outstream = new PrintWriter(System.err);
    private static String m_outputprefix = "";
@@ -76,7 +79,9 @@ public class Debug {
    private static int m_traceLevel = 3;
 
    private static final String ASSERT_FAILED_MSG = "ASSERTION FAILED: ";
-
+   
+   private static DebugFrame m_frame;
+   
    /**
    * Sets whether debugging is currently enabled. This has no effect if the 
    * TRACE_LEVEL_X constants are all false.
@@ -89,18 +94,28 @@ public class Debug {
    * Determine whether debugging is currently enabled.
    * @return true if debugging messages are currently being displayed
    */
-   public static boolean isEnabled() { return TRACE_LEVEL_1 && TRACE_LEVEL_2 && TRACE_LEVEL_3 && m_showmessage; }
+   public static boolean isEnabled() { return (TRACE_LEVEL_1 || TRACE_LEVEL_2 || TRACE_LEVEL_3) && m_showmessage; }
 
   /**
-   * Sets the debug output to use a window for output messages. The Debug
-   * object will take care of this window internally, but doesn't know when
-   * to remove the window. If you call setStream() with a new stream, the
-   * window is removed, so calling setStream(null) at the end of your
-   * program is proabably a good idea if you use a windowed output stream.
+   * Sets the debug output to use a window for output messages.
    */
   public static void useWindow(String title) {
      m_usingFrame = true;
-     m_outstream = new PrintWriter(new OutputStreamFrame(title));
+     if (m_frame == null)
+        m_frame =  new DebugFrame(DEBUG_WINDOW, title);
+     else
+        m_frame.clear();
+     m_outstream = new PrintWriter(m_frame.getStream());
+     m_frame.pack();
+     m_frame.setVisible(true);
+  }
+  
+  public static void closeWindow()
+  {
+     if (m_usingFrame)
+     {
+        m_frame.setVisible(false);   
+     }
   }
 
    /**
@@ -108,11 +123,27 @@ public class Debug {
     * output at runtime. 
     * @param trace all messages of level <= trace are displayed.
     */
-   public static void setTraceLevel(int trace)
-   {
+   public static synchronized void setTraceLevel(int trace)
+   { 
+      println("DEBUG: Set trace level = "+trace);
       m_traceLevel = trace;
    }
+   
+   public static synchronized int getTraceLevel()
+   {
+      return m_traceLevel;
+   }
 
+   public static synchronized void setAssertEnabled(boolean b)
+   {
+      println("DEBUG: Set asserts enabled = "+b);
+      m_assert = b;
+   }
+   
+   public static synchronized boolean isAssertEnabled()
+   {
+      return m_assert && ASSERT;
+   }
 
   /**
    * Set the stream to which debugging messages will be sent. By default,
@@ -210,7 +241,7 @@ public class Debug {
    */
   public static void assert(boolean condition, Class caller, String message)
   {
-     if (!condition)
+     if (m_assert && !condition)
      {
         String oldPrefix = m_outputprefix;
         m_outputprefix = ASSERT_FAILED_MSG;
@@ -250,6 +281,7 @@ public class Debug {
      Debug.println("Hello");
      Debug.println("Albsdflknweoifn");
   }
+  
 
 
 }
