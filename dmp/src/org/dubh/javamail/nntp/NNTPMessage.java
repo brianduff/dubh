e5e9cebbf0a6f46af85a86caf06a63940619afa4
Brian Duff <cairndubh@gmail.com>
1999-06-08 22:45:58 +0000

    First compiling version of the message class.

--- dmp/src/org/dubh/javamail/nntp/NNTPMessage.java
+++ dmp/src/org/dubh/javamail/nntp/NNTPMessage.java
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------------
 //   Dubh Mail Providers
-//   $Id: NNTPMessage.java,v 1.1.1.1 1999-06-06 23:37:38 briand Exp $
+//   $Id: NNTPMessage.java,v 1.2 1999-06-08 22:45:58 briand Exp $
 //   Copyright (C) 1999  Brian Duff
 //   Email: dubh@btinternet.com
 //   URL:   http://www.btinternet.com/~dubh
@@ -29,18 +29,149 @@ package dubh.mail.nntp;
 import javax.mail.*;
 import javax.mail.internet.*;
 
+import java.util.Date;
+import java.io.IOException;
+
 /**
- * A USENET news article for JavaMail
+ * A USENET news article for JavaMail.
  *
  * @author <a href="mailto:dubh@btinternet.com">Brian Duff</a>
- * @version $Id: NNTPMessage.java,v 1.1.1.1 1999-06-06 23:37:38 briand Exp $
+ * @version $Id: NNTPMessage.java,v 1.2 1999-06-08 22:45:58 briand Exp $
  */
 class NNTPMessage extends MimeMessage
 {
+   private boolean m_bXOver;
+   private String m_strMessageID;
+
+   NNTPMessage(Session s)
+   {
+      super(s);
+   }
+
+   /**
+    * The NNTPStore uses this to set the headers of this message if
+    * the server supports the XOVER extension
+    */
+   void setXOverHeaders(InternetHeaders head)
+   {
+      headers = head;
+      m_bXOver = true;
+   }
+
+   /**
+    * Get the body of the message.
+    */
+   public Object getContent()
+      throws MessagingException,
+             IOException
+   {
+      if (content == null)
+      {
+         content = getServer().getContent(this);
+      }
+      
+      return super.getContent();
+   }
    
+   
+   public void setRecipients(RecipientType type, Address[] addresses)
+      throws MessagingException
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+   
+   public void addRecipients(RecipientType type, Address[] addresses)
+      throws MessagingException
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+
+   
+   public void setReplyTo()
+      throws MessagingException
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+   
+   public void setSubject(String s)
+      throws MessagingException
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+   
+   public void setSubject(String s, String charset)
+      throws MessagingException
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+   
+   
+   public void setSentDate(Date d)
+      throws MessagingException
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+   
+   public void setReceivedDate(Date d)
+   {
+      throw new IllegalStateException("Folder is read only");
+   }
+   
+   /**
+    * Override this to force XOver header to be used, or full
+    * headers to be read in if the header doesn't exist.
+    */   
+   public String[] getHeader(String s)
+      throws MessagingException
+   {
+      loadHeader(s);
+      return super.getHeader(s);
+   }
+
+   /**
+    * Override this to force XOver header to be used, or full
+    * headers to be read in if the header doesn't exist.
+    */      
+   public String getHeader(String s, String delimiter)
+      throws MessagingException
+   {
+      loadHeader(s);
+      return super.getHeader(s, delimiter);
+   }   
+   
+   
+   /**
+    * Fetch article headers from the server if necessary. Not required
+    * if X-Over headers have already been fetched, but only if the
+    * server supports the specified header as an XOver header. You
+    * can pass null as the header to force all headers to be read in.
+    */
+   private void loadHeader(String header)
+   {
+      if (header != null && m_bXOver && !getServer().supportsXOverHeader(header))
+      {
+         headers = null;
+      }
+      if (headers == null)
+      {
+         headers = getServer().getHeaders(this);
+         m_bXOver = false;
+      }
+   }
+
+   /**
+    * Get the store for this article as an NNTPServer.
+    */
+   private NNTPStore getServer()
+   {
+      return ((NNTPStore)folder.getStore());
+   }
 }
 
 
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1.1.1  1999/06/06 23:37:38  briand
+// Dubh Mail Protocols initial revision.
+//
 //
