672dfc5a779c33db3f01296e36e3b5bbf89acff8
Brian Duff <cairndubh@gmail.com>
1999-10-24 00:44:38 +0000

    Interface changes.

--- na/src/org/dubh/apps/newsagent/navigator/NavigatorServiceProvider.java
+++ na/src/org/dubh/apps/newsagent/navigator/NavigatorServiceProvider.java
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------------
 //   NewsAgent: A Java USENET Newsreader
-//   $Id: NavigatorServiceProvider.java,v 1.1 1999-10-17 17:03:58 briand Exp $
+//   $Id: NavigatorServiceProvider.java,v 1.2 1999-10-24 00:44:38 briand Exp $
 //   Copyright (C) 1997-9  Brian Duff
 //   Email: bduff@uk.oracle.com
 //   URL:   http://st-and.compsoc.org.uk/~briand/newsagent/
@@ -26,8 +26,12 @@
 package dubh.apps.newsagent.navigator;
 
 import javax.mail.Folder;
+import javax.mail.MessagingException;
+import javax.mail.Store;
 
 import javax.swing.Icon;
+
+import dubh.utils.misc.UserPreferences;
 
 /**
  * A navigator service provider contains a (possibly nested)
@@ -38,28 +42,49 @@ import javax.swing.Icon;
  * Folders are JavaMail Folder objects.
  *
  * @author Brian Duff (dubh@btinternet.com)
- * @version $Id: NavigatorServiceProvider.java,v 1.1 1999-10-17 17:03:58 briand Exp $
+ * @version $Id: NavigatorServiceProvider.java,v 1.2 1999-10-24 00:44:38 briand Exp $
  */
-public interface NavigatorServiceProvider  
-{   
+public interface NavigatorServiceProvider extends NavigatorNode
+{ 
+   public UserPreferences getPreferences();
+  
    /**
     * Get the root folder, which is assumed to contain all other
     * folders on the service. The root folder is usually not displayed.
     */
-   public Folder getRootFolder(); 
+   public Folder getRootFolder() throws MessagingException; 
 
+   /**
+    * Set the internal name of the service. This will be used by
+    * the NavigatorService to set your service instance's name
+    * when the service provider is instantiated.
+    */
+   public void setName(String name);
+
+   /**
+    * The NavigatorService will register itself with all it's child
+    * providers after instantiation.
+    */
+   public void setService(NavigatorService s);
+
+   public NavigatorService getService();
+   
    /**
     * Get the (internal) name of the service.
     */
-   public String getServiceName();
+   public String getName();
 
    /**
     * Get the nice name of the service as it will be displayed to the user.
     * This can be the same as the internal name if you like.
     */
-   public String getServiceNiceName();
+   public String getNiceName();
+   
 }
 
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  1999/10/17 17:03:58  briand
+// Initial revision.
+//
 //
