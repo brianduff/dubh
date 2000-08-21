3f8560374ec758bf78cfdba25a817a8fd596c3cc
Brian Duff <cairndubh@gmail.com>
2000-08-21 00:31:56 +0000

    Windows build file

--- devtools/bin/buildmodule.bat
+++ devtools/bin/buildmodule.bat
@@ -1,9 +1,9 @@
 rem BuildModule, the NT version
 rem
-rem $Id: buildmodule.bat,v 1.4 2000-08-21 00:29:41 briand Exp $
+rem $Id: buildmodule.bat,v 1.5 2000-08-21 00:31:56 briand Exp $
 
 set BUILD_DIR=.
-set DT=devtools/lib
+set DT=devtools\lib
 # All this stuff is needed to build Dubh tools - most of it is for the XSLT
 # transformations for UI and RTS.
 set EXTRA_CLASSPATH=%DT%\devtools-1.0.jar;%DT%\jaxp.jar;%DT%\parser.jar:%DT%\ant.jar:%DT%\xalan_1_0_1.jar:%DT%\optional.jar:%DT%\xerces_1_0_3.jar
@@ -17,7 +17,7 @@ rem ANT_OPTIONS="-logfile $1.build.log"
 cd %BUILD_DIR%
 rem rmdir /s/q %1
 cvs checkout %1/build.xml
-set CLASSPATH=%EXTRA_CLASSPATH%:%CLASSPATH%
+set CLASSPATH=%EXTRA_CLASSPATH%;%CLASSPATH%
 java -Dant.home=%ANT_HOME% %ANT_MAIN% %ANT_OPTIONS% -buildfile %1/build.xml %2
 
 # TODO: add logging etc.
