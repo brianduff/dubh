aeb90775b4e56dffd42e2b56970d68c0d6b599e3
Brian Duff <cairndubh@gmail.com>
2001-02-11 15:49:01 +0000

    This will eventually provide an interface to the stuff that GlobalState used to do.

---
+++ na/src/org/dubh/apps/newsagent/NewsAgentAppContext.java
@@ -0,0 +1,38 @@
+// ---------------------------------------------------------------------------
+//   NewsAgent
+//   $Id: NewsAgentAppContext.java,v 1.1 2001-02-11 15:49:01 briand Exp $
+//   Copyright (C) 1997 - 2001  Brian Duff
+//   Email: Brian.Duff@oracle.com
+//   URL:   http://www.dubh.org
+// ---------------------------------------------------------------------------
+// Copyright (c) 1997 - 2001 Brian Duff
+//
+// This program is free software.
+//
+// You may redistribute it and/or modify it under the terms of the
+// license as described in the LICENSE file included with this
+// distribution.  If the license is not included with this distribution,
+// you may find a copy on the web at 'http://www.dubh.org/license'
+//
+// THIS SOFTWARE IS PROVIDED AS-IS WITHOUT WARRANTY OF ANY KIND,
+// NOT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY. THE AUTHOR
+// OF THIS SOFTWARE, ASSUMES _NO_ RESPONSIBILITY FOR ANY
+// CONSEQUENCE RESULTING FROM THE USE, MODIFICATION, OR
+// REDISTRIBUTION OF THIS SOFTWARE.
+// ---------------------------------------------------------------------------
+//   Original Author: Brian Duff
+//   Contributors:
+// ---------------------------------------------------------------------------
+//   See bottom of file for revision history
+package org.dubh.apps.newsagent;
+
+/**
+ * This interface describes the overall application context for NewsAgent.
+ * <P>
+ * @author Brian Duff
+ */
+public interface NewsAgentAppContext
+{
+}
+
+ 
