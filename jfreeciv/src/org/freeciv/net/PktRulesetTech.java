c24d1018650aeec9f9315c4fba8bdde0bc6d2797
Brian Duff <cairndubh@gmail.com>
2001-07-08 23:24:47 +0000

    Remove help stuff.

--- jfreeciv/src/org/freeciv/net/PktRulesetTech.java
+++ jfreeciv/src/org/freeciv/net/PktRulesetTech.java
@@ -1,21 +1,24 @@
 package org.freeciv.net;
-import org.freeciv.client.HelpItem;
-import org.freeciv.client.HelpPanel;
-public class PktRulesetTech extends AbstractPacket implements HelpItem
+
+
+public class PktRulesetTech extends AbstractPacket
 {
-  // Updated for 1.9.0
   public int id;
-  public int[] req = new int[ 2 ]; /* indices for advances[] */ //ptr?
+  public int[] req = new int[ 2 ]; 
   public String name;
   public int flags;
+  public String helpText;
+  
   public PktRulesetTech() 
   {
     super();
   }
+  
   public PktRulesetTech( InStream in ) 
   {
     super( in );
   }
+  
   public void receive( InStream in )
   {
     id = in.readUnsignedByte();
@@ -28,30 +31,16 @@ public class PktRulesetTech extends AbstractPacket implements HelpItem
       helpText = in.readZeroString();
     }
   }
+  
   public void send( OutStream out )
                throws java.io.IOException
   {
     
   }
+  
   public String toString()
   {
     return name;
   }
-  String helpText;
-  public void setHelpText( String txt )
-  {
-    helpText = txt;
-  }
-  public javax.swing.JComponent getRenderer( HelpPanel help )
-  {
-    return help.getTechHelpPanel( this );
-  }
-  public String getHelpCategory()
-  {
-    return "TECHS";
-  }
-  public String getHelpName()
-  {
-    return name;
-  }
+
 }
