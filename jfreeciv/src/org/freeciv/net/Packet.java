6110310d3671981c3f7d63541b81b1feec528edb
Brian Duff <cairndubh@gmail.com>
2001-07-08 00:31:25 +0000

    New interface that is nicer to pass around than AbstractPacket.

---
+++ jfreeciv/src/org/freeciv/net/Packet.java
@@ -0,0 +1,31 @@
+package org.freeciv.net;
+
+import java.io.IOException;
+
+/**
+ * All packets implement this interface.
+ */
+public interface Packet 
+{
+  /**
+   * Implement this method in your subclass to send the contents of
+   * the packet to the peer. You should implement this for all packets
+   * so that this code can be used for either a client or a server.
+   */
+  public abstract void send(OutStream os) throws IOException;
+
+  /**
+   * Implement this method in your subclass to recieve the contents
+   * of the packet from the peer and initialize internal data. You should
+   * implement this for all packets so that this code can be used for either
+   * a client or a server.
+   */
+  public abstract void receive(InStream is);
+
+  /**
+   * Get the type of this packet.
+   */
+  public int getType();
+
+  public void setType(int type);
+}
