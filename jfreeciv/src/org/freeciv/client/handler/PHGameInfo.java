87e282a714cfac1b7997e67caca5fd056b7da2f2
Brian Duff <cairndubh@gmail.com>
2001-07-12 01:02:24 +0000

    Completely rewrote based on the c code.

--- jfreeciv/src/org/freeciv/client/handler/PHGameInfo.java
+++ jfreeciv/src/org/freeciv/client/handler/PHGameInfo.java
@@ -1,12 +1,16 @@
 package org.freeciv.client.handler;
-import org.freeciv.client.*;
-import org.freeciv.client.dialog.*;
-import org.freeciv.net.*;
-import javax.swing.JOptionPane;
+
+import org.freeciv.client.Client;
+import org.freeciv.client.Constants;
+import org.freeciv.common.Game;
+import org.freeciv.net.Packet;
+import org.freeciv.net.PktGameInfo;
+
+
 /**
  * Game info handler
  */
-public class PHGameInfo implements ClientPacketHandler
+final class PHGameInfo implements ClientPacketHandler, Constants
 {
   public String getPacketClass()
   {
@@ -16,7 +20,78 @@ public class PHGameInfo implements ClientPacketHandler
    */
   public void handle( Client c, Packet pkt )
   {
-    PktGameInfo pgi = (PktGameInfo)pkt;
-    c.setGameInfo( pgi );
+    PktGameInfo pinfo = (PktGameInfo)pkt;
+    Game g = c.getGame();
+
+    g.setGold(pinfo.gold);
+    g.setTech(pinfo.tech);
+    g.setResearchCost(pinfo.researchcost);
+    g.setSkillLevel(pinfo.skill_level);
+    g.setTimeout((pinfo.timeout != 0));
+
+    g.setEndYear(pinfo.end_year);
+    g.setYear(pinfo.year);
+    g.setMinPlayers(pinfo.min_players);
+    g.setMaxPlayers(pinfo.max_players);
+    g.setNumberOfPlayers(pinfo.nplayers);
+    g.setGlobalWarming(pinfo.globalWarming);
+    g.setHeating(pinfo.heating);
+    g.setNuclearWinter(pinfo.nuclearwinter);
+    g.setCooling(pinfo.cooling);
+
+    if ( c.getGameState() == CLIENT_GAME_RUNNING_STATE )
+    {
+      g.setCurrentPlayer( pinfo.player_idx );
+      // Don't need to set pointer here - Game will handle this internally.
+    }
+
+    for ( int i=0; i < A_LAST; i++ )
+    {
+      g.setGlobalAdvance( i, pinfo.global_advances[i] );
+    }
+
+    for ( int i=0; i < B_LAST; i++ )
+    {
+      g.setGlobalWonder( i, pinfo.global_wonders[i] );
+    }
+
+    if ( c.getGameState() != CLIENT_GAME_RUNNING_STATE )
+    {
+      if ( c.getGameState() == CLIENT_SELECT_RACE_STATE )
+      {
+        c.getDialogManager().getNationDialog().undisplay();
+      }
+    }
+
+    g.setTechPenalty( pinfo.techpenalty );
+    g.setFoodBox( pinfo.foodbox );
+    g.setCivStyle( pinfo.civstyle );
+    g.setUnhappySize( pinfo.unhappysize );
+    g.setCityFactor( pinfo.cityfactor );
+
+    boolean bootHelp = (c.getState() == CLIENT_GAME_RUNNING_STATE &&
+      g.isSpaceRace() != pinfo.spacerace);
+
+    g.setSpaceRace( pinfo.spacerace );
+
+    if ( g.isTimeout() )
+    {
+      if ( pinfo.seconds_to_turndone != 0 )
+      {
+        //c.setSecondsToTurnDone( pinfo.seconds_to_turndone );
+      }
+    }
+    else
+    {
+      //c.setSecondsToTurnDone( 0 );
+    }
+
+    if ( bootHelp )
+    {
+      // boot_help_texts()
+    }
+
+    // update_unit_focus()
+    
   }
 }
--- jfreeciv/src/org/freeciv/client/handler/PHPlayerInfo.java
+++ jfreeciv/src/org/freeciv/client/handler/PHPlayerInfo.java
@@ -1,44 +1,156 @@
 package org.freeciv.client.handler;
-import org.freeciv.client.ui.CivInfoPanel;
-import org.freeciv.client.*;
-import org.freeciv.client.dialog.*;
-import org.freeciv.net.*;
 
+
+import org.freeciv.client.Client;
+import org.freeciv.client.Constants;
+import org.freeciv.common.DiplomacyState;
+import org.freeciv.common.Government;
 import org.freeciv.common.Nation;
+import org.freeciv.common.Player;
+import org.freeciv.net.Packet;
+import org.freeciv.net.PktPlayerInfo;
 
-import javax.swing.SwingUtilities;
 /**
  * Game info handler
  */
-public class PHPlayerInfo implements ClientPacketHandler
+public class PHPlayerInfo implements ClientPacketHandler, Constants
 {
   public String getPacketClass()
   {
     return "org.freeciv.net.PktPlayerInfo";
   }
+  
   /**
    */
   public void handle( final Client c, Packet pkt )
   {
-    final PktPlayerInfo ppi = (PktPlayerInfo)pkt;
-    c.setPlayer( ppi.playerno, ppi );
-    if( ppi.playerno == c.getGameInfo().player_idx )
+    PktPlayerInfo pinfo = (PktPlayerInfo) pkt;
+    
+    Player p = c.getGame().getPlayer( pinfo.playerno );
+
+    p.setName( pinfo.name );
+    p.setNation( (Nation)
+      c.getFactories().getNationFactory().findById( pinfo.nation )
+    );
+    p.setMale( pinfo.is_male );
+
+    p.getEconomy().setGold( pinfo.gold );
+    p.getEconomy().setTax( pinfo.tax );
+    p.getEconomy().setScience( pinfo.science );
+    p.getEconomy().setLuxury( pinfo.luxury );
+
+    p.setGovernment( (Government)
+      c.getFactories().getGovernmentFactory().findById( pinfo.government )
+    );
+    p.setEmbassy( pinfo.embassy );
+    p.setGivesSharedVision( pinfo.gives_shared_vision );
+    p.setCityStyle( pinfo.city_style );
+
+
+    for ( int i=0; i < MAX_NUM_PLAYERS + MAX_NUM_BARBARIANS; i++)
     {
-      c.setCurrentPlayer( ppi );
-      SwingUtilities.invokeLater( new Runnable() 
+      DiplomacyState ds = p.getDiplomacyState(i);
+      ds.setType( pinfo.diplstates[i].getType() );
+      ds.setTurnsLeft( pinfo.diplstates[i].getTurnsLeft() );
+      ds.setReasonToCancel( pinfo.diplstates[i].hasReasonToCancel() );
+    }
+
+    p.setReputation( pinfo.reputation );
+
+    for (int i=0; i < MAX_NUM_WORKLISTS; i++)
+    {
+      p.setWorkList( i, pinfo.worklists[i] );
+    }
+
+    for(int i=0; i < c.getGame().getNumberOfTechnologyTypes(); i++)
+    {
+      p.getResearch().setHasInvention( i, pinfo.inventions[ i ] ); // ?
+    }
+
+    // c.updateResearch( p );
+
+    //?
+    boolean popTechUp = p.getResearch().getCurrentlyResearching().getId() != 
+      pinfo.researching;
+
+    p.getResearch().setResearched( pinfo.researched );
+    p.getResearch().setResearchPoints( pinfo.researchpoints );
+    p.getResearch().setCurrentlyResearching( pinfo.researching ); // lookup advance?
+
+    p.setFutureTech( pinfo.future_tech );
+    p.getAI().setTechGoal( pinfo.tech_goal );
+
+    if ( c.getGameState() == CLIENT_GAME_RUNNING_STATE && 
+      c.getGame().isCurrentPlayer( p ) )
+    {
+      if (popTechUp)
       {
-        public void run()
+        if ( !p.getAI().isControlled() ) // || ai_popup_windows
         {
-          CivInfoPanel p = c.getCivInfoPanel();
-          p.setGold( ppi.gold );
-          p.setTax( ppi.tax, ppi.science, ppi.luxury );
-          p.setNationName( 
-            ((Nation)c.getFactories().getNationFactory().findById(ppi.nation)).
-              getName()
-          );
-        // TODO: Update research dialog.
+          // c.popupScienceDialog( 1 );
         }
-      } );
+        // did_advance_tech_this_turn = c.getGame().getYear();  ??
+        // c.updateScienceDialog();
+
+        // if we just learned bridge building and focus is on a settler on
+        // a river, the road menu item will remain disabled unless we do
+        // this. 
+
+        // if (c.isUnitFocused())
+        // {
+        //   c.updateMenus();
+        // }
+      }
+    }
+
+    p.setTurnDone( pinfo.turn_done );
+    p.setNumberOfIdleTurns( pinfo.nturns_idle );
+    p.setAlive( pinfo.is_alive );
+
+    p.setConnected( pinfo.is_connected );
+
+    p.getAI().setBarbarian( pinfo.is_barbarian );
+    p.setRevolution( pinfo.revolution );
+
+    if ( p.getAI().isControlled() != pinfo.ai )
+    {
+      p.getAI().setControlled( pinfo.ai );
+      
+      if ( c.getGame().isCurrentPlayer( p ) )
+      {
+        c.appendOutputWindow( 
+          "AI Mode is now "+
+            (p.getAI().isControlled() ? "ON" : "OFF")
+        );
+      }
+    }
+
+    if ( c.getGame().isCurrentPlayer( p ) &&
+         (p.getRevolution() < 1 || p.getRevolution() > 5) &&
+         p.getGovernment() == c.getGame().getGovernmentWhenAnarchy() &&
+         (!p.getAI().isControlled() || true ) && // ai_popup_windows
+         c.getGameState() == CLIENT_GAME_RUNNING_STATE )
+    {
+      // c.popupGovernmentDialog();
+    }
+
+    // c.updatePlayersDialog();
+    // c.updateWorklistReportDialog();
+
+    if ( c.getGame().isCurrentPlayer( p ) )
+    {
+      if ( c.getGameState() == CLIENT_GAME_RUNNING_STATE )
+      {
+        if ( !p.isTurnDone() )
+        {
+          // c.setTurnDoneButtonEnabled( true );
+        }
+        else
+        {
+          // c.updateTurnDoneButton();
+        }
+        c.updateInfoLabel();
+      }
     }
   }
 }
