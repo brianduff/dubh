9564f8d72122f7b05c3861bc572569cdeb7939ec
Brian Duff <cairndubh@gmail.com>
2001-07-12 00:58:02 +0000

    Will soon be obsoleted (has moved to common)

--- jfreeciv/src/org/freeciv/net/PlayerDiplomacyState.java
+++ jfreeciv/src/org/freeciv/net/PlayerDiplomacyState.java
@@ -1,39 +1,9 @@
 package org.freeciv.net;
 import org.freeciv.util.Enum;
+/**
+ * @obsolete
+ */
 public class PlayerDiplomacyState
 {
-  public int DS_NEUTRAL = Enum.start();
-  public int DS_WAR = Enum.get();
-  public int DS_CEASEFIRE = Enum.get();
-  public int DS_PEACE = Enum.get();
-  public int DS_ALLIANCE = Enum.get();
-  public int DS_NO_CONTACT = Enum.get();
-  public int DS_LAST = Enum.get(); /* leave this last */
-  private int m_type;
-  private int m_turnsLeft;
-  private int m_reasonToCancel;
-  public int getType()
-  {
-    return m_type;
-  }
-  public void setType( int i )
-  {
-    m_type = i;
-  }
-  public int getTurnsLeft()
-  {
-    return m_turnsLeft;
-  }
-  public void setTurnsLeft( int turnsLeft )
-  {
-    m_turnsLeft = turnsLeft;
-  }
-  public void setReasonToCancel( int reasonToCancel )
-  {
-    m_reasonToCancel = reasonToCancel;
-  }
-  public int getReasonToCancel()
-  {
-    return m_reasonToCancel;
-  }
+
 }
