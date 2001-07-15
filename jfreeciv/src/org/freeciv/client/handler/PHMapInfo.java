e49167df414cb3ca81f1172f3f9306ced8c95e71
Brian Duff <cairndubh@gmail.com>
2001-07-15 00:58:30 +0000

    Populate the game map and show the overview map.

--- jfreeciv/src/org/freeciv/client/handler/PHMapInfo.java
+++ jfreeciv/src/org/freeciv/client/handler/PHMapInfo.java
@@ -1,22 +1,44 @@
 package org.freeciv.client.handler;
-import org.freeciv.client.*;
-import org.freeciv.client.dialog.*;
-import org.freeciv.net.*;
-import javax.swing.JOptionPane;
+
+import org.freeciv.client.Client;
+import org.freeciv.common.Map;
+import org.freeciv.net.Packet;
+import org.freeciv.net.PktMapInfo;
+
 /**
  * Map info handler
  */
 public class PHMapInfo implements ClientPacketHandler
 {
+
   public String getPacketClass()
   {
     return "org.freeciv.net.PktMapInfo";
   }
+  
   /**
    */
   public void handle( Client c, Packet pkt )
   {
     PktMapInfo pmi = (PktMapInfo)pkt;
+  //  c.createMap( pmi.xsize, pmi.ysize, pmi.isEarth );
+    Map map = c.getGame().getMap();
+    
+    map.setWidth( pmi.xsize );
+    map.setHeight( pmi.ysize );
+    map.setEarth( pmi.isEarth );
+
+    map.allocate();
+    c.initContinents();
+
+    c.getMapView().setOverviewDimensions( pmi.xsize, pmi.ysize );
+
+
+    // How is this done in the c client???
+    c.showOverviewMap();
+    
+    // Remove me 
     c.createMap( pmi.xsize, pmi.ysize, pmi.isEarth );
+    
   }
 }
