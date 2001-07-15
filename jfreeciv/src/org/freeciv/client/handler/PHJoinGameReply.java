6c72d416cc38f19275c29eead533fd5eb3e98a3e
Brian Duff <cairndubh@gmail.com>
2001-07-15 00:59:26 +0000

    Stop setting server caps on client. print capabilities to the log window if caps don't match on client and server.

--- jfreeciv/src/org/freeciv/client/handler/PHJoinGameReply.java
+++ jfreeciv/src/org/freeciv/client/handler/PHJoinGameReply.java
@@ -8,10 +8,12 @@ import javax.swing.JOptionPane;
  */
 public class PHJoinGameReply implements ClientPacketHandler
 {
+
   public String getPacketClass()
   {
     return "org.freeciv.net.PktJoinGameReply";
   }
+  
   /**
    */
   public void handle( Client c, Packet pkt )
@@ -23,8 +25,17 @@ public class PHJoinGameReply implements ClientPacketHandler
       c.appendOutputWindow( "You were rejected from the game: " + p.message );
       return ;
     }
-    c.appendOutputWindow( p.message );
-    c.setServerCapabilities( p.capabilities );
-    c.setGameState( Constants.CLIENT_PRE_GAME_STATE );
+
+    c.setGameState( Constants.CLIENT_PRE_GAME_STATE ); //?
+
+    if (c.getCapabilities().equals(p.capabilities))
+    {
+      return;
+    }
+    
+    c.appendOutputWindow( "Client capability string: "+ c.getCapabilities() );
+    c.appendOutputWindow( "Server capability string: "+ p.capabilities );   
+    
+
   }
 }
