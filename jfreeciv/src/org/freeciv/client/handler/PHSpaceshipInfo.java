1968ed61275fd414e8a17c4659ff2fe19b75f73d
Brian Duff <cairndubh@gmail.com>
2001-07-15 00:58:05 +0000

    Populate spaceship info at the start of the game.

--- jfreeciv/src/org/freeciv/client/handler/PHSpaceshipInfo.java
+++ jfreeciv/src/org/freeciv/client/handler/PHSpaceshipInfo.java
@@ -2,7 +2,9 @@ package org.freeciv.client.handler;
 
 import org.freeciv.client.handler.ClientPacketHandler;
 import org.freeciv.client.Client;
+import org.freeciv.common.Player;
 import org.freeciv.net.Packet;
+import org.freeciv.net.PktSpaceshipInfo;
 
 /**
  * Packet handler for spaceship_info packets.
@@ -19,6 +21,22 @@ final class PHSpaceshipInfo implements ClientPacketHandler
    */
   public void handle( final Client c, final Packet pkt )
   {
-    
+    PktSpaceshipInfo p = (PktSpaceshipInfo) pkt;
+    Player player = c.getGame().getPlayer( p.player_num );
+
+    player.getSpaceship().initFromPacket( pkt );
+
+    if ( !c.getGame().isCurrentPlayer( player ) )
+    {
+      // c.refreshSpaceshipDialog(); ??
+      return;
+    }
+
+    // c.updateMenus();
+
+    // if (!c.getSpaceship().isAutoplace())
+    // {
+    //   c.refreshSpaceshipDialog();
+    // }
   }
 }
