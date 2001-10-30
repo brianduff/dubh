db02740a6e9cab04b6858d6c4c5923dc42718497
Brian Duff <cairndubh@gmail.com>
2001-10-30 23:14:23 +0000

    Added code to move the map to a known square at the start of the game. Still needs to be refined when we know about units etc. [bduff]

--- jfreeciv/src/org/freeciv/client/handler/PHGameState.java
+++ jfreeciv/src/org/freeciv/client/handler/PHGameState.java
@@ -2,13 +2,18 @@ package org.freeciv.client.handler;
 
 import org.freeciv.client.Client;
 import org.freeciv.client.Constants;
+import org.freeciv.common.Game;
+import org.freeciv.common.Map;
+import org.freeciv.common.MapPosition;
+import org.freeciv.common.MapPositionIterator;
+import org.freeciv.common.Tile;
 import org.freeciv.net.Packet;
 import org.freeciv.net.PktGenericInteger;
 
 /**
  * Handle the "game state" packet
  */
-public class PHGameState implements ClientPacketHandler,  Constants
+public class PHGameState extends AbstractHandler implements Constants
 {
 
   /**
@@ -24,7 +29,7 @@ public class PHGameState implements ClientPacketHandler,  Constants
    * Your handler should implement this method to actually
    * handle an incoming packet.
    */
-  public void handle( Client c, Packet pkt )
+  public void handleOnEventThread( final Client c, final Packet pkt )
   {
     PktGenericInteger packet = (PktGenericInteger) pkt;
     
@@ -40,10 +45,38 @@ public class PHGameState implements ClientPacketHandler,  Constants
     if (c.getGameState() == CLIENT_GAME_RUNNING_STATE)
     {
       c.getMainWindow().getMapOverview().refresh();
+      c.updateInfoLabel();
+
+
+      // TODO: Clever tile centering algorithm, as per handle_game_state..
+      // Until normal service is resumed, however, find any old known tile...
+      int hx = c.getGame().getMap().getWidth() / 2;
+      int hy = c.getGame().getMap().getHeight() / 2;
+      c.getGame().getMap().iterateOutwards( hx, hy, Math.max( hx, hy ), 
+        new MapPositionIterator() 
+        {
+          public void iteratePosition( MapPosition pos )
+          {
+            Game g = c.getGame();
+            Map m = g.getMap();
+            Tile t = m.getTile( pos.x, pos.y );
+            if ( t.isKnown() )
+            {
+              c.getMainWindow().getMapViewManager().centerOnTile(
+                pos.x, pos.y
+              );
+              setFinished( true );
+            }
+          }
+        }
+      );
+
+      
+      //c.getMainWindow().getMapViewManager().centerOnTile( 40, 25 );  //TODO
       // enable_turn_done_button()
       // player_set_unit_focus_status()
 
-      c.updateInfoLabel();
+
 
       // update_unit_focus()
       // update_unit_info_label();
