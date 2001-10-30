4575446bf60b0e050298da93da4ed8d56d74f5f3
Brian Duff <cairndubh@gmail.com>
2001-10-30 20:23:25 +0000

    Fix deprecation warning in StdIn [briand]

--- jfreeciv/src/org/gjt/abies/StdIn.java
+++ jfreeciv/src/org/gjt/abies/StdIn.java
@@ -11,44 +11,45 @@
 
 package org.gjt.abies;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class StdIn
 {
 
-	public final static DataInputStream in =
-		new DataInputStream(System.in);
+   public final static BufferedReader in =
+      new BufferedReader( new InputStreamReader(System.in) );
 
-	public static final int YES = 1;
-	public static final int NO = -1;
+   public static final int YES = 1;
+   public static final int NO = -1;
 
-	public static String readLine()
-	{
-		try {
-			return in.readLine();
-		} catch ( IOException e )
-			{
-				return null;
-			}
-	}
+   public static String readLine()
+   {
+      try {
+         return in.readLine();
+      } catch ( IOException e )
+         {
+            return null;
+         }
+   }
 
-	public static int readYesNo()
-	{
-		while ( true )
-		{
-			String a = readLine();
-			if ( a.equals(_("yes")) || a.equals(_("_yes.short")) )
-				return YES;
-			if ( a.equals(_("no")) || a.equals(_("_no.short")) )
-				return NO;
-			System.out.println(_("You have to answer yes or no"));
-		}
-	}
+   public static int readYesNo()
+   {
+      while ( true )
+      {
+         String a = readLine();
+         if ( a.equals(_("yes")) || a.equals(_("_yes.short")) )
+            return YES;
+         if ( a.equals(_("no")) || a.equals(_("_no.short")) )
+            return NO;
+         System.out.println(_("You have to answer yes or no"));
+      }
+   }
 
-	private static String _(String str)
-	{
-		return Localize.translation.translate(str);
-	}
+   private static String _(String str)
+   {
+      return Localize.translation.translate(str);
+   }
 
 }
