76bd3d644e9b0693da524f72f5a5cbe6f851c555
Brian Duff <cairndubh@gmail.com>
2001-11-11 21:19:27 +0000

    [removed]

--- jfreeciv/src/org/freeciv/client/HelpItem.java
+++
@@ -1,9 +0,0 @@
-package org.freeciv.client;
-import javax.swing.JComponent;
-public interface HelpItem
-{
-  public String getHelpCategory();
-  public String getHelpName();
-  void setHelpText( String txt );
-  JComponent getRenderer( HelpPanel help );
-}
